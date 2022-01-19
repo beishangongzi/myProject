62bf8bca5c8f1ff97707d953269b4c66bff5203b
Andy Z Wright <zhangruibin021@gmail.com>
2022-01-19 17:23:53 +0800

    自定义权限和认证

--- myProject/settings.py
+++ myProject/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'generic_viewset',
     'mixin_generic_viewset',
     'mixin_generic_viewset_router',
+    'opt',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -140,3 +141,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'opt.authentication.CustomAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
