ae41045368515d9640a651270518e5b3851073d6
Andy Z Wright <zhangruibin021@gmail.com>
2022-01-21 15:02:04 +0800

    备注genericViewSet

--- generic_viewset/views.py
+++ generic_viewset/views.py
@@ -11,10 +11,20 @@ class StudentListView(View):
     serializer_class = StudentSerializer
 
     def list(self, request):
+        """
+        获取所有实体
+        :param request:
+        :return:
+        """
         serializers = self.serializer_class(self.get_queryset(), many=True)
         return Response(serializers.data)
 
     def create(self, request):
+        """
+        创建一个实体
+        :param request:
+        :return:
+        """
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(True)
         instance = serializer.save()
@@ -22,22 +32,46 @@ class StudentListView(View):
 
 
     def retrieve(self, request, pk):
+        """
+        获取id是pk的实体
+        :param request:
+        :param pk:
+        :return:
+        """
         instance = self.get_object()
         return Response(self.get_serializer(instance).data)
 
     def update(self, request, pk):
+        """
+        更新
+        :param request:
+        :param pk:
+        :return:
+        """
         instance = self.get_object()
         serializer = self.get_serializer(instance, request.data)
         serializer.is_valid(True)
         return Response(self.get_serializer(serializer.save()).data)
 
     def partial_update(self, request, pk):
+        """
+        部分更新
+        :param request:
+        :param pk:
+        :return:
+        """
         instance = self.get_object()
         serializer = self.get_serializer(instance, request.data, partial=True)
         serializer.is_valid(True)
         return Response(self.get_serializer(serializer.save()).data)
 
     def destroy(self, request, pk):
+        """
+        删除某个实体
+        :param request:
+        :param pk:
+        :return:
+        """
         instance = self.get_object()
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
