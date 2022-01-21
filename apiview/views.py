1bbfc1ace44c129b023345fcc4d70f24d9673e2c
Andy Z Wright <zhangruibin021@gmail.com>
2022-01-21 14:35:00 +0800

    备注apiview

--- apiview/views.py
+++ apiview/views.py
@@ -5,9 +5,23 @@ from rest_framework import status
 from .serializers import StudentSerializer
 from students.models import Student
 
+# 使用APIView和view的两个区别就在于
+# 1. 需要导入from rest_framework.views import APIView
+# 2. request这个参数 将不再是之前的request，而是被drf处理的request
+# 3. 可以直接使用Response返回数据， 不在需要JsonResponse了
 
 class StudentListView(View):
     def get(self, request):
+        """
+        得到所有的学生信息
+        :param request: request扩展了DJango中的request。
+            request.data 返回的是前段传入的body。就像Django中的request.POST, request.FILES
+                        他可以返回所有body数据，包括文件和非文件
+            .query_params: request.GET。 因为并不是只有get方法可以通过地址栏传参，所以drf中重新写了这个函数
+            .user: 用户
+            如果想使用原生的request，这样调用： request._request.
+        :return:
+        """
         students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data)
